033a7e34f406d1d6841c1e596b1483dc346ff22c
codenameMR <tldgkgud00@gmail.com>
2023-02-14 11:27:13 +0900

    복구

---
+++ _musinsa_teamProject/src/main/java/com/musinsa/fashionBoard/controller/FashionBoroadController.java
@@ -0,0 +1,67 @@
+package com.musinsa.fashionBoard.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.musinsa.fashionBoard.mapper.FashionBoardMapper;
+import com.musinsa.fashionBoard.model.FashionBoardDTO;
+
+@Controller
+public class FashionBoroadController {
+
+	FashionBoardMapper fm;
+	
+	@Autowired
+	public FashionBoroadController(FashionBoardMapper fm) {
+		super();
+		this.fm = fm;
+	}
+
+
+	int i = 0;
+	@GetMapping("/insert")
+	public String test(FashionBoardDTO dto) {
+		dto.setUser_id("testid"+i);
+		i++;
+		fm.Insert(dto);
+		return "redirect:fashionBoard";
+	}
+	
+	@GetMapping("/fashionBoard")
+	public String fashionBoard(Model model) {
+		List<FashionBoardDTO> flist = fm.selectAll();
+		model.addAttribute("flist", flist);
+		return "fashionBoard";
+	}
+	
+	@GetMapping("/fashionBoardOne")
+	public String fashionBoardOne(Model model, int board_num) {
+		FashionBoardDTO fd = fm.selectOne(board_num);
+		model.addAttribute("selectOne",fd);
+		return "fashionBoardOne";
+	}
+	
+	@GetMapping("/fashionBoardUpdate")
+	public void fashionBoardUpdate(Model model, int board_num) {
+		FashionBoardDTO fd = fm.selectOne(board_num);
+		model.addAttribute("selectOne",fd);
+	}
+	
+	@PostMapping("/fashionBoardUpdate")
+	public String fashionBoardUpdate(FashionBoardDTO fd) {
+		fm.updateBoard(fd);
+		return "redirect:/fashionBoard";
+	}
+	
+	@GetMapping("/fashionBoardDelete")
+	public String fashionBoardDelete(int board_num) {
+		fm.delete(board_num);
+		return "redirect:/fashionBoard";
+	}
+	
+}
